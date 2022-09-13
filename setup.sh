625373ba3b45ad3c63e455a670768e41d097abdc
Johannes Egger <j.egger@posteo.at>
2022-09-13 06:04:44 +0200

    Install docker compose on target during setup

--- setup.sh
+++ setup.sh
@@ -4,9 +4,13 @@
 #   * Set host name, enable SSH, set username/password, configure WIFI, set locale settings
 
 sudo apt update
-sudo apt install --no-install-recommends -y git
+sudo apt install -y git
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 rm get-docker.sh
+
+sudo apt update
+sudo apt install -y python python-pip
+sudo pip install docker-compose
