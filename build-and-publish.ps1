4199c7765061a5d3f49bee58c660a51b4bab0ae6
Johannes Egger <j.egger@posteo.at>
2022-09-18 20:25:10 +0200

    Build tailwind before publish

--- build-and-publish.ps1
+++ build-and-publish.ps1
@@ -1,4 +1,5 @@
 $version = "0.1"
+./build-tailwind.ps1
 docker build -t johannesegger/musiscore:$version -t johannesegger/musiscore:latest ./src/
 docker push johannesegger/musiscore:$version
 docker push johannesegger/musiscore:latest
