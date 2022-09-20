f98829720f0ff9b934ec35b0a46c1978b1d4626c
Johannes Egger <j.egger@posteo.at>
2022-09-20 19:41:26 +0200

    Build docker image for amd64 and arm32 and support testing in local container

--- build-and-publish.ps1
+++ build-and-publish.ps1
@@ -1,5 +1,3 @@
-$version = "0.1"
+$version = "0.5"
 ./build-tailwind.ps1
-docker build -t johannesegger/musiscore:$version -t johannesegger/musiscore:latest ./src/
-docker push johannesegger/musiscore:$version
-docker push johannesegger/musiscore:latest
+docker buildx build --push --platform linux/amd64,linux/arm/v7 --tag johannesegger/musiscore:$version --tag johannesegger/musiscore:latest ./src/
