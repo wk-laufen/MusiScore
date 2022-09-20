c27df400eb5579f016a35aa6ebcbc8cbdc9cc494
Johannes Egger <j.egger@posteo.at>
2022-09-20 11:02:29 +0200

    Explicitly specify print media format

--- src/MusiScore.Server/Printer.fs
+++ src/MusiScore.Server/Printer.fs
@@ -6,7 +6,7 @@ module Printer =
     let printPdf content printSetting count = async {
         let printOptions =
             match printSetting with
-            | Duplex -> "-o sides=two-sided-long-edge"
+            | Duplex -> "-o media=A4 -o sides=two-sided-long-edge"
             | A4ToA3Duplex
             | A4ToBooklet -> "-o number-up=2 -o media=A3 -o sides=two-sided-long-edge"
         use! temporaryFile = TemporaryFile.create ".pdf" content
