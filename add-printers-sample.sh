47cdc360b59ce31e99aaf37554a9c063d377aa10
Johannes Egger <j.egger@posteo.at>
2022-09-21 05:53:36 +0200

    Extract samples on how to add printers with CUPS

--- add-printers-sample.sh
+++ add-printers-sample.sh
@@ -1,6 +1,4 @@
-docker run -d -p 631:631 ydkn/cups
-
 # see https://www.cups.org/doc/admin.html
 # lpadmin -p Brother-MFC-9120CN -E -L L201 -m openprinting-ppds:0/ppd/openprinting/Brother/BR9120_2_GPL.ppd -v ipp://MFC-9120CN:631/ipp
 # lpadmin -p Ricoh-Aficio-3025 -E -L MuZi -m gutenprint.5.3://ricoh-afc_3025/expert -v ipp://192.168.2.10:631/ipp
-#  -> TODO set A4 as default
+# lpadmin -p Ricoh-Aficio-3025 -o PageSize-default=A4 # doesn't seem to work because UI still says "Letter" is the default
