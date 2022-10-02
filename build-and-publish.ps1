d31c9d39df8f1f0b4c0c4ad6522e2fdc401ae255
Johannes Egger <j.egger@posteo.at>
2022-10-02 14:23:02 +0200

    Publish 0.8

--- build-and-publish.ps1
+++ build-and-publish.ps1
@@ -1,4 +1,4 @@
-$version = "0.7"
+$version = "0.8"
 ./build-tailwind.ps1
 # docker buildx create --use # create a new build context once
 docker buildx build --push --platform linux/amd64,linux/arm/v7 --tag johannesegger/musiscore:$version --tag johannesegger/musiscore:latest ./src/
