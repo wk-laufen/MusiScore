2b58fb1cad827298614bda7e21521f0b3ecc4495
Johannes Egger <j.egger@posteo.at>
2024-01-06 07:57:46 +0100

    Use ASP.NET minimal API

--- src/MusiScore.Server/Startup.fs
+++ src/MusiScore.Server/Startup.fs
@@ -1,74 +1,75 @@
-namespace MusiScore.Server
+module MusiScore.Server.App
 
-open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Authentication.Cookies
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open System
 open System.IO
+open System.Reflection
+open System.Text.Json.Serialization
 
-type Startup() =
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    member this.ConfigureServices(services: IServiceCollection) =
-        services.AddSingleton<Db>(fun (serviceProvider: IServiceProvider) ->
-            let connectionString =
-                let config = serviceProvider.GetService<IConfiguration>()
-                config.GetConnectionString("Db")
+    builder.Services.AddSingleton<Db>(fun (serviceProvider: IServiceProvider) ->
+        let connectionString =
+            let config = serviceProvider.GetService<IConfiguration>()
+            config.GetConnectionString("Db")
+            |> Option.ofObj
+            |> Option.orElseWith (fun () ->
+                config.GetValue("Db_ConnectionString_File")
                 |> Option.ofObj
-                |> Option.orElseWith (fun () ->
-                    config.GetValue("Db_ConnectionString_File")
-                    |> Option.ofObj
-                    |> Option.map File.ReadAllText
-                )
-                |> Option.defaultWith (fun () -> failwith "DB connection string not found")
-            Db(connectionString)
-        ) |> ignore
-
-        services.AddSingleton<Printer>(fun (serviceProvider: IServiceProvider) ->
-            let printConfig = serviceProvider.GetService<IConfiguration>().GetSection("Print")
-            let printServer = printConfig.GetValue("Server")
-            let printerName = printConfig.GetValue("Printer")
-            Printer(printServer, printerName)
-        ) |> ignore
-
-        services.AddMvc() |> ignore
-        services.AddServerSideBlazor() |> ignore
-        services
-            .AddAuthorization()
-            .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie()
-                .Services
-        |> ignore
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
-        if env.IsDevelopment() then
-            app.UseDeveloperExceptionPage() |> ignore
-
-        app
-            .UseAuthentication()
-            .UseStaticFiles()
-            .UseRouting()
-            .UseBlazorFrameworkFiles()
-            .UseEndpoints(fun endpoints ->
-                endpoints.MapControllers() |> ignore
-                endpoints.MapFallbackToFile("index.html") |> ignore
+                |> Option.map File.ReadAllText
             )
+            |> Option.defaultWith (fun () -> failwith "DB connection string not found")
+        Db(connectionString)
+    ) |> ignore
+
+    builder.Services.AddSingleton<Printer>(fun (serviceProvider: IServiceProvider) ->
+        let printConfig = serviceProvider.GetService<IConfiguration>().GetSection("Print")
+        let printServer = printConfig.GetValue("Server")
+        let printerName = printConfig.GetValue("Printer")
+        Printer(printServer, printerName)
+    ) |> ignore
+
+    builder.Services
+        .AddControllers()
+        .AddJsonOptions(fun o ->
+            Assembly.GetExecutingAssembly().ExportedTypes
+            |> Seq.filter(fun v -> v.BaseType <> null && not v.ContainsGenericParameters && typeof<JsonConverter>.IsAssignableFrom(v))
+            |> Seq.map Activator.CreateInstance
+            |> Seq.cast<JsonConverter>
+            |> Seq.iter o.JsonSerializerOptions.Converters.Add
+        ) |> ignore
+
+    builder.Services.AddMvc() |> ignore
+    builder.Services.AddServerSideBlazor() |> ignore
+    builder.Services.AddAuthorization() |> ignore
+    builder.Services
+        .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie()
+    |> ignore
+
+    let app = builder.Build()
+
+    if app.Environment.IsProduction() then
+        app
+            .UseDefaultFiles()
+            .UseStaticFiles()
         |> ignore
 
-module Program =
+    app.UseAuthentication() |> ignore
+    app.UseAuthorization() |> ignore
+    app.MapControllers() |> ignore
 
-    [<EntryPoint>]
-    let main args =
-        WebHost
-            .CreateDefaultBuilder(args)
-            .UseStaticWebAssets()
-            .UseStartup<Startup>()
-            .Build()
-            .Run()
-        0
+    if app.Environment.IsProduction() then
+        app.UseRouting().UseEndpoints(fun endpoints ->
+            endpoints.MapFallbackToFile("/index.html") |> ignore
+        ) |> ignore
+
+    app.Run()
+
+    0
