4bb0d43361bca1d3945773394b34f4c3ec3ec105
Johannes Egger <j.egger@posteo.at>
2024-08-25 07:16:33 +0200

    Show loading indicator when loading PDF document or rendering PDF pages

--- src/MusiScore.Client/src/components/PdfPage.vue
+++ src/MusiScore.Client/src/components/PdfPage.vue
@@ -1,43 +1,58 @@
 <script setup lang="ts">
-import { ref, toRaw, watch } from 'vue'
+import { computed, ref, toRaw, watch } from 'vue'
 import { RenderingCancelledException, type PDFDocumentProxy } from 'pdfjs-dist'
+import LoadingBar from './LoadingBar.vue'
 
 const props = defineProps<{
   pdfDoc: PDFDocumentProxy
-  pageNumber: number,
+  pageNumber: number
+  isLoadingDocument: boolean
 }>()
 
 const container = ref<HTMLCanvasElement>()
+const isLoadingPage = ref(false)
 watch([() => props.pdfDoc, () => props.pageNumber, container], async ([pdfDoc, pageNumber, c], _, onCleanup) => {
   if (c === undefined) return
 
-  const p = await toRaw(pdfDoc).getPage(pageNumber)
-  const drawingContext = c.getContext('2d')
-  if (drawingContext === null) return
-
-  const viewport = p.getViewport({ scale: 1 })
-
-  const outputScale = window.devicePixelRatio || 1
-  c.width = Math.floor(viewport.width * outputScale)
-  c.height = Math.floor(viewport.height * outputScale)
-
-  const renderTask = toRaw(p).render({ canvasContext: drawingContext, viewport })
-  onCleanup(() => renderTask.cancel())
   try {
-    await renderTask.promise
+    isLoadingPage.value = true
+    const p = await toRaw(pdfDoc).getPage(pageNumber)
+    const drawingContext = c.getContext('2d')
+    if (drawingContext === null) return
+
+    const viewport = p.getViewport({ scale: 1 })
+
+    const outputScale = window.devicePixelRatio || 1
+    c.width = Math.floor(viewport.width * outputScale)
+    c.height = Math.floor(viewport.height * outputScale)
+
+    const renderTask = toRaw(p).render({ canvasContext: drawingContext, viewport })
+    onCleanup(() => renderTask.cancel())
+    try {
+      await renderTask.promise
+    }
+    catch (e) {
+      if (e instanceof RenderingCancelledException) {
+        console.log('Page rendering cancelled')
+      }
+      else {
+        console.error(e)
+        // TODO notify user
+      }
+    }
   }
-  catch (e) {
-    if (e instanceof RenderingCancelledException) {
-      console.log('Page rendering cancelled')
-    }
-    else {
-      console.error(e)
-      // TODO notify user
-    }
+  finally {
+    isLoadingPage.value = false
   }
 })
+
+const isLoading = computed(() => props.isLoadingDocument || isLoadingPage.value)
 </script>
 
 <template>
-  <canvas ref="container" class="border"></canvas>
+  <div class="grid">
+    <canvas ref="container" class="row-span-full col-span-full border"></canvas>
+    <div v-if="isLoading" class="row-span-full col-span-full bg-white opacity-50"></div>
+    <LoadingBar v-if="isLoading" type="minimal" class="row-span-full col-span-full place-self-center"></LoadingBar>
+  </div>
 </template>
--- src/MusiScore.Client/src/components/PdfPreview.vue
+++ src/MusiScore.Client/src/components/PdfPreview.vue
@@ -12,25 +12,32 @@ const props = defineProps<{
 }>()
 
 const pdfDoc = ref<PDFDocumentProxy>()
+const isLoadingDocument = ref(false)
 watch(() => props.file, async (file, _, onCleanup) => {
   if (file === undefined) {
     pdfDoc.value = undefined
     return
   }
   
-  const loadDocTask = getDocument(file.slice())
-  onCleanup(() => loadDocTask.destroy())
   try {
-    pdfDoc.value = await loadDocTask.promise
+    isLoadingDocument.value = true
+    const loadDocTask = getDocument(file.slice())
+    onCleanup(() => loadDocTask.destroy())
+    try {
+      pdfDoc.value = await loadDocTask.promise
+    }
+    catch (e) {
+      if (e instanceof Error && e.message === 'Worker was destroyed') {
+        console.log('PDF document loading cancelled')
+      }
+      else {
+        console.error('PDF document loading failed', e)
+        // TODO notify user
+      }
+    }
   }
-  catch (e) {
-    if (e instanceof Error && e.message === 'Worker was destroyed') {
-      console.log('PDF document loading cancelled')
-    }
-    else {
-      console.error('PDF document loading failed', e)
-      // TODO notify user
-    }
+  finally {
+    isLoadingDocument.value = false
   }
 }, { immediate: true })
 
@@ -39,6 +46,9 @@ watch(() => props.file, async (file, _, onCleanup) => {
 
 <template>
   <div v-if="pdfDoc !== undefined" class="flex gap-2 flex-wrap items-center">
-    <PdfPage v-for="pageNumber in range(1, pdfDoc.numPages + 1)" :key="pageNumber" :pdf-doc="pdfDoc" :page-number="pageNumber" />
+    <PdfPage v-for="pageNumber in range(1, pdfDoc.numPages + 1)" :key="pageNumber"
+      :pdf-doc="pdfDoc"
+      :page-number="pageNumber"
+      :is-loading-document="isLoadingDocument" />
   </div>
 </template>
