f5e5f2663cb7234ccadbca62a2987ad33f8b1a81
Johannes Egger <j.egger@posteo.at>
2025-06-05 20:13:11 +0200

    Put CUPS libs in /usr/lib instead of /usr/lib64

--- cups-docker/docker-entrypoint.sh
+++ cups-docker/docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="/usr/lib64:$LD_LIBRARY_PATH" && ldconfig
-
-echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
-ls -la /usr/lib64
-echo "/etc/ld.so.conf:"
-cat /etc/ld.so.conf
-echo "/etc/ld.so.conf.d/*:"
-cat /etc/ld.so.conf.d/*
-
 /usr/sbin/cupsd \
 && while [ ! -f /var/run/cups/cupsd.pid ]; do sleep 1; done \
 && cupsctl --remote-admin --remote-any --share-printers \
