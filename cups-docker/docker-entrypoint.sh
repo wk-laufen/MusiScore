7ac73ce331fd38382b1346b552d6eeffd4f63169
Johannes Egger <j.egger@posteo.at>
2025-06-05 08:46:39 +0200

    Build and use OpenPrinting/cups docker image

---
+++ cups-docker/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH="/usr/lib64:$LD_LIBRARY_PATH" && ldconfig
+
+/usr/sbin/cupsd \
+&& while [ ! -f /var/run/cups/cupsd.pid ]; do sleep 1; done \
+&& cupsctl --remote-admin --remote-any --share-printers \
+&& kill $(cat /var/run/cups/cupsd.pid) \
+&& echo "ServerAlias *" >> /etc/cups/cupsd.conf \
+&& service cups start \
+&& /usr/sbin/cupsd -f
