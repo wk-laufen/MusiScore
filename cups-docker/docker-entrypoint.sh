97a5cfbb5ae115eb3f4b536ebe6d7e964f8e3e4e
Johannes Egger <j.egger@posteo.at>
2025-06-05 14:46:53 +0200

    Clean up

--- cups-docker/docker-entrypoint.sh
+++ cups-docker/docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
-ls -la /usr/lib64
-
 /usr/sbin/cupsd \
 && while [ ! -f /var/run/cups/cupsd.pid ]; do sleep 1; done \
 && cupsctl --remote-admin --remote-any --share-printers \
