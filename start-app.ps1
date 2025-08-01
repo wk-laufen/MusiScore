7fabadf2967cba1fea4897f79c24a54a565365bc
Johannes Egger <j.egger@posteo.at>
2025-08-01 09:16:33 +0200

    Use VSCode tasks instead of start-app.ps1

--- start-app.ps1
+++
@@ -1,4 +0,0 @@
-wt `
-    new-tab --startingDirectory . `-`- docker compose -f .\docker-compose.dev.yml up`; `
-    new-tab --startingDirectory .\src\MusiScore.Server `-`- dotnet watch run --environment Development --urls=https://localhost:5001`; `
-    new-tab --startingDirectory .\src\MusiScore.Client `-`- npm.cmd install `&`& npm.cmd run dev
